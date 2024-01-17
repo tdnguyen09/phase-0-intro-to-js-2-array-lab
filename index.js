cedd70e9be4d3d1b12a455f079320e8842c1dd56
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-01-18 01:14:37 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+    return cats;
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+    return cats;
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    return cats;
+}
+function appendCat(name){
+    let newCatsArray = [...cats, name]
+    return newCatsArray;
+}
+function prependCat(name){
+    let newCatsArray = [name, ...cats]
+    return newCatsArray;
+}
+
+function removeLastCat(){
+    let newCatsArray = cats.slice(0,2)
+    return newCatsArray;
+}
+
+function removeFirstCat(){
+    let newCatsArray = cats.slice(1)
+    return newCatsArray;
+}
